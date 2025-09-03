10bc3f60f86865cc7f6ff4dad39ab389f2d28286
kurorinchan <kurorinchan@users.noreply.github.com>
2025-09-03 23:34:16 +0900

    Fix build with gh-pages
    also fix mime type

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // GitHub Pagesでプロジェクトをデプロイするために、ベースパスを設定します。
+  // ここでは、リポジトリ名と同じパスを使用します。
+  base: '/artifact-routine-tracker/',
   plugins: [react()],
-})
+});
